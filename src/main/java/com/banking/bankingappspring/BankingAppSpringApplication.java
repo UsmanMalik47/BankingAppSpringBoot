e1ce67abc40ae60231294b9a3327a988ad2b51e8
Usman <f_breath@hotmail.com>
2024-04-12 17:45:45 +0500

    Project Start

---
+++ src/main/java/com/banking/bankingappspring/BankingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.banking.bankingappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppSpringApplication.class, args);
+	}
+
+}
